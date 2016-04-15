f03fc2ba4672c60b50c9334dc19cd905c249b482
Ricardo Taborda <ricardotaborda@gmail.com>
2016-04-15 14:43:25 -0500

    a compiling version for work with the bengal basin

--- quake/forward/bengalbasin.h
+++ quake/forward/bengalbasin.h
@@ -8,6 +8,10 @@
 #ifndef BENGALBASIN_H_
 #define BENGALBASIN_H_
 
-float* bengal_cvm_query(FILE *fp, float east_m, float north_m, float depth_m);
+typedef struct theprops_t {
+    float Vp, Vs, rho;
+} theprops_t;
+
+int bengal_cvm_query(FILE *fp, float east_m, float north_m, float depth_m, theprops_t* result);
 
 #endif /* BENGALBASIN_H_ */
